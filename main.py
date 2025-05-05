609225028150f01209fc1b2dfabb7dc2b4d43d0b
owm <enless143@gmail.com>
2025-05-05 19:17:45 +0300

    Fix parameters and UI components Add Harris Corner Detector

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os, cv2
 import numpy as np
+import tkinter as tk
 from tkinter     import filedialog
 from tkinter     import *
 from tkinter.ttk import *
@@ -17,52 +18,69 @@ class Application:
 		self.img_proc=   None #Processed image buffer
 		self.img_chosen= None #Reference to image chosen for drawing 
 		self.img_canvas= None #Canvas image item id
-		self.hb_active=  False
 		self.zoom_scale= 1.0
 		self.filters= [
 			'Normalize', 'Grayscale', 
 			'Sharpen', 'Gaussian Blur',
 			'Edge Detection',
-			'Detection w/SIFT',
+			'Corner Detection',# Harris
 			'SIFT keypoints',
-			'Harris Corner Detector',
+			'Detection w/SIFT', #TODO: fix this so it properly detects all objects in the objects folder separately
+			'Detection w/CNN',  #TODO: implement this, preferably with segmentation preprocessing somehow
+			'Detection w/RCNN', #TODO: might scrap this one
 		]
 
 	# INIT UI ELEMENTS (order doesn't affect element position)
 		self.canvas=            Canvas(root, bg='gray')
-		self.ctrlpanel=         Frame(self.root)
+		self.ctrlpanel=         Frame(root)
 		self.fltr_params=       Frame(self.ctrlpanel)
 		self.fltr_list_title=   Label(self.ctrlpanel, text='Available Functions')
 		self.fltr_params_title= Label(self.ctrlpanel, text='Parameters')
 		self.fltr_var=          Variable(value=self.filters)
 		self.fltr_list=         Listbox(self.ctrlpanel, selectmode=SINGLE, listvariable=self.fltr_var,      state=DISABLED)
 		self.applyb=            Button(self.ctrlpanel, text='Apply',           command=self.fltr_apply,     state=DISABLED)
-		# self.rb=                Button(self.ctrlpanel, text='Revert Original', command=self.img_rvrtorigin, state=DISABLED)
-		# self.hb=                Button(self.ctrlpanel, text='Show Original',   command=self.img_showorigin, state=DISABLED)
+		self.rb=                Button(self.ctrlpanel, text='Revert Original', command=self.img_rvrtorigin, state=DISABLED)
+		self.hb=                Button(self.ctrlpanel, text='Show Original',   command=None,                state=DISABLED)
 
 	# DRAW UI ELEMENTS (ORDER MATTERS)
-		self.canvas.pack(fill=BOTH, expand=1)
-		self.ctrlpanel.pack(fill=BOTH, side=RIGHT)
-		self.fltr_list_title.pack(anchor=NW, expand=1)
-		self.fltr_list.pack(fill=BOTH, expand=1)
-		# self.rb.pack()
-		# self.hb.pack()
-		self.fltr_params_title.pack(side=LEFT, anchor=NW)
-		self.fltr_params.pack(fill=BOTH, side=LEFT, expand=1)
+		self.canvas.pack(fill=BOTH, side=LEFT, expand=1, anchor=NW)
+		self.ctrlpanel.pack(fill=BOTH, side=RIGHT, anchor=NE)
+		self.fltr_list_title.pack(anchor=NW)
+		self.fltr_list.pack(fill=BOTH)
 		self.applyb.pack(side=BOTTOM)
+		self.rb.pack(pady=1)
+		self.hb.pack(pady=1)
+		self.fltr_params_title.pack(anchor=NW)
+		self.fltr_params.pack(fill=BOTH, side=LEFT, expand=1)
 
 	# FILTER PARAMETERS
 		self.param_sharpness=     DoubleVar(value=1.0)
 		self.param_blur=          DoubleVar(value=1.0)
 		self.param_canny_thresh1= IntVar(value=100)
 		self.param_canny_thresh2= IntVar(value=200)
+		self.param_harris_bsize=  IntVar(value=2)
+		self.param_harris_ksize=  IntVar(value=3)
+		self.param_harris_k=      DoubleVar(value=0.04)
 
 		self.canvas.bind("<MouseWheel>", self.zoom)
 		self.canvas.bind("<Button-1>",   self.img_load)
 		self.fltr_list.bind('<<ListboxSelect>>', self.fltr_select)
-		# self.hb.bind('<ButtonPress-1>',   self.holdbutton_active)
-		# self.hb.bind('<ButtonRelease-1>', self.holdbutton_inactive)
+		self.hb.bind('<ButtonPress-1>',   self.holdbutton_active)
+		self.hb.bind('<ButtonRelease-1>', self.holdbutton_inactive)
 
+	def holdbutton_active(self, event=None):
+		self.img_chosen= self.img_orig
+		self.img_display()
+
+	def holdbutton_inactive(self, event=None):
+		self.img_chosen= self.img_proc
+		self.img_display()
+	
+	def img_rvrtorigin(self, event=None):
+		self.img_proc= self.img_orig.copy()
+		self.img_chosen= self.img_proc
+		self.img_display()
+	
 	def fltr_apply(self):
 		idxs= self.fltr_list.curselection()
 		if len(idxs)>0:
@@ -137,8 +155,13 @@ class Application:
 					if len(matches)>20:
 						img= cv2.drawMatches(img, keypoints1, obj_img, keypoints2, matches[:20], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 						img= cv2.putText(img, obj_image, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-			elif filter=='Harris Corner Detector':
-				pass #TODO: detects all corner points in the image
+			elif filter=='Harris Corner Detection':
+				if len(img.shape)==3: gray= cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+				else:                 gray= img
+				harris_response= cv2.cornerHarris(gray, self.param_harris_bsize.get(), self.param_harris_ksize.get(), self.param_harris_k.get())
+				dilated_corners= cv2.dilate(harris_response, None)
+				threshold= 0.01*harris_response.max()
+				img[dilated_corners>threshold]= [0, 255, 0]# Mark corners in green
 			else:
 				return
 
@@ -159,16 +182,20 @@ class Application:
 
 			self.applyb.config(state=NORMAL)
 			if   filter=='Sharpen':
-				Scale(self.fltr_params, from_=0.1, to=2, orient='horizontal', variable=self.param_sharpness).pack()
+				tk.Scale(self.fltr_params, label='Sharpness', from_=0.1, to=2, resolution=0.1, orient='horizontal', variable=self.param_sharpness).pack()
 			elif filter=='Gaussian Blur':
-				Scale(self.fltr_params, from_=0.1, to=10, orient='horizontal', variable=self.param_blur).pack()
+				tk.Scale(self.fltr_params, label='Sigma', from_=0.1, to=10, resolution=0.1, orient='horizontal', variable=self.param_blur).pack()
 			elif filter=='Edge Detection':
-				Scale(self.fltr_params, from_=1, to=254, orient='horizontal', variable=self.param_canny_thresh1).pack()
-				Scale(self.fltr_params, from_=1, to=254, orient='horizontal', variable=self.param_canny_thresh2).pack()
+				tk.Scale(self.fltr_params, label='Thresh1', from_=0, to=512, orient='horizontal', variable=self.param_canny_thresh1).pack()
+				tk.Scale(self.fltr_params, label='Thresh2', from_=0, to=512, orient='horizontal', variable=self.param_canny_thresh2).pack()
 			elif filter=='Normalize':        pass #no params
 			elif filter=='Grayscale':        pass #no params
 			elif filter=='SIFT keypoints':   pass #no params
-			elif filter=='Detection w/SIFT': pass #TODO: pass a reference image of object to detect
+			elif filter=='Detection w/SIFT': pass #no params (use objects folder for reference object images)
+			elif filter=='Harris Corner Detection':
+				tk.Scale(self.fltr_params, label='Block Size',  from_=2, to=10, orient='horizontal', variable=self.param_harris_bsize).pack()
+				tk.Scale(self.fltr_params, label='Kernel Size', from_=1, to=9,   resolution=2,   orient='horizontal',  variable=self.param_harris_ksize).pack()
+				tk.Scale(self.fltr_params, label='k',           from_=0, to=0.1, resolution=0.01, orient='horizontal', variable=self.param_harris_k).pack()
 			else:
 				self.applyb.config(state=DISABLED)
 				print(f'[INFO]: Filter {filter} not implemented.')
@@ -182,8 +209,8 @@ class Application:
 			self.img_display()
 			
 			self.fltr_list.config(state=NORMAL)
-			# self.rb.config(state=NORMAL)
-			# self.hb.config(state=NORMAL)
+			self.rb.config(state=NORMAL)
+			self.hb.config(state=NORMAL)
 
 	def img_display(self):
 		w, h= self.img_chosen.size
